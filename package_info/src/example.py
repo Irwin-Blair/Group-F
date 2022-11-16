54336672fafb66cd89151be05e046060500758cc
Becca <rebecca.mallett2@ntlworld.com>
2022-11-16 16:36:04 +0000

    changing the example because i used the wrong file

--- package_info/src/example.py
+++ package_info/src/example.py
@@ -1,37 +1,89 @@
+"""
+Created on Fri Nov 11 10:38:23 2022
+@author: iarla
+"""
+
 import numpy as np
-from scipy.sparse.linalg import inv, eigs
-from scipy.sparse import csc_matrix, eye, diags
 import matplotlib.pyplot as plt
 from findiff import FinDiff
+from scipy.sparse.linalg import inv
+from scipy.sparse import eye, diags
+import matplotlib.animation as animation
+from Potential_Function import User_Defined_Potential, KindInput 
 
-#setting up a grid
-x = np.linspace(-5,5,100)
-t = np.linspace(0,5,100)
-dt = t[1] - t[0]
+plt.rcParams["axes.labelsize"] = 16
 
+# Allowing user input for the placement for the wave function on the plot
+print("Please input desired wave placement:")
+# Using the function KindInput created by Irwin the user can input any float for the wave placement
+wave_placement = float(KindInput(float))
+print(type(wave_placement))
+# Input parameters
+Nx = 500
+xmin = -5
+xmax = 5
 
+Nt = 250
+tmin = 0
+tmax = 20
+k = 1 
 
-#using Hamiltonian to create matrix
-H = -0.5 * FinDiff(0, x[1] - x[0], 2).matrix(x.shape) + diags(0.5*x**2)
+# Calculate grid, potential, and initial wave function
+x_array = np.linspace(xmin, xmax, Nx)
+t_array = np.linspace(tmin, tmax, Nt)
+# User defined v(x) with function User_Defined_Potential created by Irwin 
+v_x = User_Defined_Potential(x_array)[0]
+# wave function with user input placement
+psi = np.exp(-(x_array - wave_placement)**2)
 
+# Calculate finite difference elements
+dt = t_array[1] - t_array[0]
+dx = x_array[1] - x_array[0]
+
+# Convert to a diagonal matrix
+v_x_matrix = diags(v_x)
+
+# Calculate the Hamiltonian matrix
+H = -0.5 * FinDiff(0, dx, 2).matrix(x_array.shape) + v_x_matrix
+
+# Apply boundary conditions to the Hamiltonian
 H[0, :] = H[-1, :] = 0
 H[0, 0] = H[-1, -1] = 1
 
-I_plus = csc_matrix(eye(len(x)) + 1j * dt / 2. * H)
-I_minus = csc_matrix(eye(len(x)) - 1j * dt / 2. * H)
+# Calculate U
+I_plus = eye(Nx) + 1j * dt / 2. * H
+I_minus = eye(Nx) - 1j * dt / 2. * H
 U = inv(I_minus).dot(I_plus)
 
-psi = np.exp(-(x+2)**2) / np.sqrt(np.pi)
-
-
-fig = plt.figure()
-ax = fig.gca()
-ax.set_xlabel('x')
-ax.set_ylabel(r'$|\psi|$')
-ax.grid()
-
-for it, t in enumerate(t):
+# Iterate over each time, appending each calculation of psi to a list
+psi_list = []
+for t in t_array:
     psi = U.dot(psi)
     psi[0] = psi[-1] = 0
-    if it % 4 == 1:
-        ax.plot(x, abs(psi))
+    psi_list.append(np.abs(psi))
+    
+psi_mag_squared = np.abs(psi_list)**2
+
+
+fig, ax = plt.subplots()
+
+ax.set_xlabel("x [arb units]")
+ax.set_ylabel("$|\Psi(x, t)|$", color="C0")
+
+ax_twin = ax.twinx()
+ax_twin.plot(x_array, v_x, color="C1")
+ax_twin.set_ylabel("V(x) [arb units]", color="C1")
+
+line, = ax.plot([], [], color="C0", lw=2)
+ax.grid()
+xdata, ydata = [], []
+
+def run(psi):
+    line.set_data(x_array, np.abs(psi)**2)
+    return line,
+
+ax.set_xlim(x_array[0], x_array[-1])
+ax.set_ylim(0, 1)
+
+ani = animation.FuncAnimation(fig, run, psi_list, interval=10)
+ani.save("particle_in_a_well.gif", fps=120, dpi=300)
